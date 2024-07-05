086228c6818102b6bef543028182414d4bf9c752
Nishi <99659309+sahoonishi@users.noreply.github.com>
2024-07-05 23:32:25 +0530

    DAY 1

--- LongestWord.js
+++ LongestWord.js
@@ -3,9 +3,9 @@
 
 //----------------------------METHOD 1------------------------------------
 const longest=(str)=>{
-  if(str.trim().length===0) return false;
+  if(str.trim().length===0) return false; // TO REMOVE WHITESPACE FROM BOTH END OF A STRING
 
-  let arr = str.split(" ");
+  let arr = str.split(" "); // IF WE PERFORM SPLIT ON A STRING IT WILL CONVERTED TO AN ARRAY
   console.log(arr);
 
   return arr.reduce((acc,word)=> word.length > acc.length ? word : acc , arr[0])
@@ -16,9 +16,9 @@ const longest=(str)=>{
 const longestt=(str)=>{
   if(str.trim().length===0) return false;
   let arr = str.split(" ");
-  arr.sort((a,b)=> b.length - a.length);
+  arr.sort((a,b)=> b.length - a.length); // COMPARISION OPERATOR 
   console.log(arr);
-  return arr[0];
+  return arr[0]; // FOR THE LAST ELEMENT WRITE  -    arr.at(-1);
 }
 
 console.log(longest("My Name is Nishi Sahooo prajna"));
